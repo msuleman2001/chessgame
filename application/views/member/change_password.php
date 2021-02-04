66f05e32bc6d450422aa49a10cc64a98fcc46114
msuleman2001 <msuleman2001@gmail.com>
2021-02-04 15:02:35 +0500

    change password page

--- application/views/member/change_password.php
+++ application/views/member/change_password.php
@@ -6,8 +6,13 @@
 		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
 		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/fa/css/all.css">
 	</head>
-	<body>
-		your code here
+	<body class="body-bg">
+		<nav class="navbar navbar-expand-lg main-navbar">
+			<div class="mr-auto"></div>
+		</nav>
+		<div class="main-art">
+			your code here
+		</div>
 		<script src="js/jquery-3.3.1.min.js"></script>
 		<script src="js/scripts.js"></script>
 		<script src="js/custom.js"></script>
