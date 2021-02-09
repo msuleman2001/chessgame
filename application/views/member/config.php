4996ce51da38fad4ff97894d8bcd16205441baad
Muhammad Shahid <shahidofficial79@gmail.com>
2021-02-09 16:39:54 +0500

    Changes

---
+++ application/views/member/config.php
@@ -0,0 +1,52 @@
+<?php
+/* 
+| Developed by: Tauseef Ahmad
+| Last Upate: 13-12-2020 04:46 PM
+| Facebook: www.facebook.com/ahmadlogs
+| Twitter: www.twitter.com/ahmadlogs
+| YouTube: https://www.youtube.com/channel/UCOXYfOHgu-C-UfGyDcu5sYw/
+| Blog: https://ahmadlogs.wordpress.com/
+ */ 
+ 
+ // Include the autoloader provided in the SDK
+require_once(__DIR__.'/Facebook/autoload.php');
+
+define('APP_ID', '884326105710726');
+define('APP_SECRET', '8dd7813d2241e72001e56b75720614d3');
+define('API_VERSION', 'v1.0');
+define('FB_BASE_URL', 'https://onlinebestchessgame.000webhostapp.com/index.php/member');
+
+define('BASE_URL', 'https://onlinebestchessgame.000webhostapp.com');
+
+if(!session_id()){
+    session_start();
+}
+
+
+// Call Facebook API
+$fb = new Facebook\Facebook([
+ 'app_id' => APP_ID,
+ 'app_secret' => APP_SECRET,
+ 'default_graph_version' => API_VERSION,
+]);
+
+
+// Get redirect login helper
+$fb_helper = $fb->getRedirectLoginHelper();
+
+
+// Try to get access token
+try {
+    if(isset($_SESSION['facebook_access_token']))
+		{$accessToken = $_SESSION['facebook_access_token'];}
+	else
+		{$accessToken = $fb_helper->getAccessToken();}
+} catch(FacebookResponseException $e) {
+     echo 'Facebook API Error: ' . $e->getMessage();
+      exit;
+} catch(FacebookSDKException $e) {
+    echo 'Facebook SDK Error: ' . $e->getMessage();
+      exit;
+}
+
+?>
