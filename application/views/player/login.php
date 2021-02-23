452eeb9b483bbefb6b4be8aaba89d1cfcc9c327a
msuleman2001 <msuleman2001@gmail.com>
2021-02-23 13:29:23 +0500

    login changes

--- application/views/player/login.php
+++ application/views/player/login.php
@@ -1,82 +1,6 @@
-<?php
-/* 
-| Developed by: Tauseef Ahmad
-| Last Upate: 13-12-2020 04:46 PM
-| Facebook: www.facebook.com/ahmadlogs
-| Twitter: www.twitter.com/ahmadlogs
-| YouTube: https://www.youtube.com/channel/UCOXYfOHgu-C-UfGyDcu5sYw/
-| Blog: https://ahmadlogs.wordpress.com/
- */ 
- 
-require_once 'config.php';
-
-$permissions = ['email']; //optional
-
-if (isset($accessToken))
-{
-	if (!isset($_SESSION['facebook_access_token'])) 
-	{
-		//get short-lived access token
-		$_SESSION['facebook_access_token'] = (string) $accessToken;
-		
-		//OAuth 2.0 client handler
-		$oAuth2Client = $fb->getOAuth2Client();
-		
-		//Exchanges a short-lived access token for a long-lived one
-		$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
-		$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
-		
-		//setting default access token to be used in script
-		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-	} 
-	else 
-	{
-		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-	}
-	
-	
-	//redirect the user to the index page if it has $_GET['code']
-	if (isset($_GET['code'])) 
-	{
-		header('Location: ./');
-	}
-	
-	
-	try {
-		$fb_response = $fb->get('/me?fields=name,first_name,last_name,email');
-		$fb_response_picture = $fb->get('/me/picture?redirect=false&height=200');
-		
-		$fb_user = $fb_response->getGraphUser();
-		$picture = $fb_response_picture->getGraphUser();
-		
-		$_SESSION['fb_user_id'] = $fb_user->getProperty('id');
-		$_SESSION['fb_user_name'] = $fb_user->getProperty('name');
-		$_SESSION['fb_user_email'] = $fb_user->getProperty('email');
-		$_SESSION['fb_user_pic'] = $picture['url'];
-		
-		
-	} catch(Facebook\Exceptions\FacebookResponseException $e) {
-		echo 'Facebook API Error: ' . $e->getMessage();
-		session_destroy();
-		// redirecting user back to app login page
-		header("Location: ./");
-		exit;
-	} catch(Facebook\Exceptions\FacebookSDKException $e) {
-		echo 'Facebook SDK Error: ' . $e->getMessage();
-		exit;
-	}
-} 
-else 
-{	
-	// replace your website URL same as added in the developers.Facebook.com/apps e.g. if you used http instead of https and you used
-	$fb_login_url = $fb_helper->getLoginUrl('http://localhost/facebook1/', $permissions);
-}
-?>
 <html>
 	<head>
 		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/chess.css">
-		
-		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/stylew.css">
 		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
 		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/fa/css/all.css">
 		
@@ -87,7 +11,7 @@ else
 		<nav class="navbar navbar-expand-lg main-navbar">
 			<div class="mr-auto"></div>
 		</nav>
-		<button onclick="signOut()">Sign out</button>
+		
 		<div class="main-art">
 			<div class="two-col-left">
 				<form id="frmLogin" method="post" action="<?php echo site_url('Player/perform_login'); ?>" class="needs-validation" novalidate="">
@@ -134,12 +58,12 @@ else
 				</div>
 			</div>
 			<div class="two-col-right">
-				<img src="img_girl.jpg"  width="500" height="600">
+				
 			</div>
 		</div>
-		<script src="js/jquery-3.3.1.min.js"></script>
-		<script src="js/scripts.js"></script>
-		<script src="js/custom.js"></script>
-		<script src="js/bootstrap.min.js">
+		<script src="<?php echo base_url(); ?>assets/js/jquery-3.4.1.min.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/scripts.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/custom.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js">
 	</body>
 </html>
